0b845d1e1718796f734902f2b8b489741967c4c8
Adam Davies <adamd94@hotmail.com>
2020-07-08 21:33:22 +0100

    Complete 'Testing and Debugging' section

--- app/src/main/java/com/example/funfactsactivity/FunFactsActivity.java
+++ app/src/main/java/com/example/funfactsactivity/FunFactsActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FunFactsActivity extends AppCompatActivity {
+    private static final String TAG = FunFactsActivity.class.getSimpleName();
     private FactBook factBook = new FactBook();
     private ColorWheel colorWheel = new ColorWheel();
 
@@ -42,5 +45,8 @@ public class FunFactsActivity extends AppCompatActivity {
             }
         };
         showFactButton.setOnClickListener(listener);
+
+        //Toast.makeText(this, "Yay! Our Activity was created!", Toast.LENGTH_SHORT).show();
+        //Log.d(TAG, "We're logging from the onCreate() method!");
     }
 }
